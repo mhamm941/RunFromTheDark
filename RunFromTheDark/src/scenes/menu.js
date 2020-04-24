f2b5a1d776f17bd92fcd8a237871d254e64947b7
Matt <62917970+mhamm941@users.noreply.github.com>
2020-04-24 16:49:21 -0700

    Add files via upload

---
+++ RunFromTheDark/src/scenes/menu.js
@@ -0,0 +1,18 @@
+class menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    create() {
+
+        let centerX = game.config.width/2;
+        let centerY = game.config.height/2;
+
+        this.add.text(0, 0, "Press any key to continue").setOrigin(0.5);
+
+        this.input.keyboard.on('keydown', () => {
+            this.scene.start("playScene");
+        }, this);
+    }
+
+}
