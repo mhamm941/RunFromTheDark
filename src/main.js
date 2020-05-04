557c1bebedffc2c1e7d3dc0aee2d21e35455acaa
mhamm941 <mhamm@ucsc.edu>
2020-05-03 21:51:07 -0700

    Main comment block

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+/*
+    Run From The Dark - Endless Runner
+    Authors - Amber Vo, Matthew Hamm, Petr Luzunov
+    
+    CREATIVE TILT
+    Technical - We implimented a "life" feature that tracks how close you are to the end of the game. 
+                When you hit an obstacle, the darkness grows closer and when you clear one the darkness gets fainter.
+
+    Artistic  - Matt designed all the sounds and the music in our game. This was his first time doing this 
+                kind of sound design where he messed with distortion waveforms for the sound of the light switch and 
+                used the chrome music lab to create the background music. Amber and Petr collaborated to create the 
+                visuals for the game, keeping a haunting and cartoony style.
+*/
+
 let config = {
    // parent: 'myGame',
     type: Phaser.WEBGL,
