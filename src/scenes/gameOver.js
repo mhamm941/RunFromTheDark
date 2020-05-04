e4a43ba574db43974c80cb2a6090b920c35b1af6
mhamm941 <mhamm@ucsc.edu>
2020-05-03 18:06:29 -0700

    Credits

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -15,6 +15,23 @@ class gameOver extends Phaser.Scene {
         this.keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         this.keyM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
 
+        //credits
+        this.add.text(20, 20, "Programmers - Amber Vo, Matthew Hamm", {
+            fontFamily: "Courier",
+            fontSize: '15px',
+        }).setOrigin(0,0);
+        this.add.text(20, 40, "Artists - Amber Vo, Petr Luzunov", {
+            fontFamily: "Courier",
+            fontSize: '15px',
+        }).setOrigin(0,0);
+        this.add.text(20, 60, "Sound Designer - Matthew Hamm", {
+            fontFamily: "Courier",
+            fontSize: '15px',
+        }).setOrigin(0,0);
+        this.add.text(20, 80, "Some code derived from Nathan Altice's Paddle Parkour and Movement Studies", {
+            fontFamily: "Courier",
+            fontSize: '15px',
+        })
         this.add.text(game.config.width/2, game.config.height/2 -64, "GAME OVER", menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, "YOUR SCORE: " + playScore, menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 +64, "R to restart", menuConfig).setOrigin(0.5);
